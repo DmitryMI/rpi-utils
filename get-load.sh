7c46e0d7d2e6a839c2e668df512eaa9275aa48a0
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-04-26 13:51:12 +0200

    Order changed

--- get-load.sh
+++ get-load.sh
@@ -1,14 +1,14 @@
+# Get task list, CPU and MEM load, sorting by MEM
+# LINES=20 top -n 5 -o +%MEM -w
+
+# Get task list, CPU and MEM load, soring by CPU
+LINES=20 top -n 5 -o +%CPU -w
+
+# Get total memory
+echo "Total memory:"
+vcgencmd get_mem arm && vcgencmd get_mem gpu
+echo ""
 
 # Read cpu temperature
 vcgencmd measure_temp
 echo ""
-
-# Get total memory
-vcgencmd get_mem arm && vcgencmd get_mem gpu
-echo ""
-
-# Get task list, CPU and MEM load, sorting by MEM
-LINES=20 top -d 5.0 -n 1 -b -o +%MEM +%CPU -w
-
-# Get task list, CPU and MEM load, soring by CPU
-LINES=20 top -d 5.0 -n 1 -b -o +%CPU -w
