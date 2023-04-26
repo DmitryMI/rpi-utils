35cd3eb9d826033e2e3453b041bb2b623a092319
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-04-26 13:36:55 +0200

    Delay time added

--- get-load.sh
+++ get-load.sh
@@ -8,7 +8,7 @@ vcgencmd get_mem arm && vcgencmd get_mem gpu
 echo ""
 
 # Get task list, CPU and MEM load, sorting by MEM
-LINES=20 top -n 3 -b -o +%MEM +%CPU -w
+LINES=20 top -d 5.0 -n 1 -b -o +%MEM +%CPU -w
 
 # Get task list, CPU and MEM load, soring by CPU
-LINES=20 top -n 3 -b -o +%CPU -w
+LINES=20 top -d 5.0 -n 1 -b -o +%CPU -w
