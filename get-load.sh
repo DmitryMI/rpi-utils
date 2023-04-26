ad1d9ba00af0969e42c056b578deef84cddde67b
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-04-26 13:33:30 +0200

    Sorting changed

--- get-load.sh
+++ get-load.sh
@@ -7,5 +7,8 @@ echo ""
 vcgencmd get_mem arm && vcgencmd get_mem gpu
 echo ""
 
-# Get task list, CPU and MEM load
+# Get task list, CPU and MEM load, sorting by MEM
 LINES=20 top -n 3 -b -o +%MEM +%CPU -w
+
+# Get task list, CPU and MEM load, soring by CPU
+LINES=20 top -n 3 -b -o +%CPU -w
