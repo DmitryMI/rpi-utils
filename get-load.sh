d8594ab73eaf06ad16ce7ed004f0a93e42ea62f1
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-04-26 14:03:46 +0200

    Array index fixed

--- get-load.sh
+++ get-load.sh
@@ -24,5 +24,5 @@ gpu_freq_str="$(vcgencmd measure_clock core)"
 arm_freq_words="$(echo $arm_freq_str | tr "=" "\n")"
 gpu_freq_words="$(echo $gpu_freq_str | tr "=" "\n")"
 
-printf "CPU Frequency: \t$arm_freq_words[1] Hz"
-printf "GPU Frequency: \t$gpu_freq_words[1] Hz"
+printf "CPU Frequency: \t${arm_freq_words[1]} Hz\n"
+printf "GPU Frequency: \t${gpu_freq_words[1]} Hz\n"
